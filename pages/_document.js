e8d4d968b81e14324f5ad85d5d13a47734cca268
Sam Pettersson <sam@sampettersson.com>
2021-12-17 08:43:11 +0100

    Update

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document, { Head, Main, NextScript } from 'next/document'
+import Document, { Head, Main, NextScript, Html } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
@@ -11,7 +11,7 @@ export default class MyDocument extends Document {
 
   render () {
     return (
-      <html>
+      <Html>
         <Head>
           <title>Sam Pettersson</title>
           {this.props.styleTags}
@@ -20,7 +20,7 @@ export default class MyDocument extends Document {
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     )
   }
 }
